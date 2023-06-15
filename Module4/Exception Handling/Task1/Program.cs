5884d999ed8ae559d01bb152fc8e17f686a70475
Tatsiana Yarmolich <tatsiana_yarmolich@epam.com>
2023-06-15 10:53:47 +0300

    Added Module4

---
+++ Module4/Exception Handling/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string enteredWord;
+            do
+            {
+                OperationWithString operationWithString = new OperationWithString();
+
+                Console.WriteLine("Please, enter word");
+                enteredWord = Console.ReadLine();
+
+                try
+                {
+                    if (String.IsNullOrWhiteSpace(enteredWord))
+                    {
+                        throw new InvalidEnterException("Invalid entered value");
+                    }
+                    else
+                    {
+
+                        operationWithString.GetCharacter(enteredWord);
+                    }
+                }
+                catch (InvalidEnterException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            } while (enteredWord != "close");
+        }
+
+        public class OperationWithString
+        {
+            public void GetCharacter(string word)
+            {
+                string firstCharacter = word.Substring(0, 1);
+                Console.WriteLine(firstCharacter);
+            }
+        }
+
+        class InvalidEnterException : Exception
+        {
+            public InvalidEnterException(string message)
+                : base(message) { }
+        }
+    }
+}
---
+++ Module4/Exception Handling/Task2/NumberParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public const string EmptyValue = "Entered value can not be empty";
+        public const string InncorrectFormatValue = "Inncorrect type of entered value";
+        public const string ValueOverflowed = "Value exceeded";
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+                throw new ArgumentNullException(EmptyValue);
+
+            if (string.IsNullOrWhiteSpace(stringValue))
+                throw new FormatException(InncorrectFormatValue);
+
+            long result = 0;
+            int sighn = 1;
+
+            for (int i = 0; i < stringValue.Length; i++)
+            {
+                char c = stringValue[i];
+                stringValue = stringValue.Trim();
+                
+                if (c >= '0' && c <= '9')
+                {
+                    result = result * 10 + (c - '0');
+                }
+                else if (c == '-' && i == 0)
+                {
+                    sighn = -1;
+                }
+                else if (c == '+' && i == 0)
+                {
+                    sighn = +1;
+                }
+                else
+                {
+                    throw new FormatException(InncorrectFormatValue);
+                }
+            }
+            
+            result = result * sighn;
+
+            if (result == int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+            else if (result == int.MinValue)
+            {   
+                return int.MinValue; 
+            }
+
+            if (result > int.MaxValue || result < int.MinValue)
+            {
+                throw new OverflowException(ValueOverflowed);
+            }
+
+            int lengthMaxValue = int.MaxValue.ToString().Length;
+
+            if (stringValue.Length > lengthMaxValue)
+            {
+                throw new OverflowException(ValueOverflowed);
+            }
+
+            return (int)result ;
+
+        }
+    }
+}
