358eedfdecc9f6aef11ccaad615274f45834bde8
Tatsiana Yarmolich <tatsiana_yarmolich@epam.com>
2023-06-15 10:52:08 +0300

    Added Module2

---
+++ Module2/Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        
+        public double Price { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Product p = (Product)obj;
+                return (Name == p.Name) && (Price == p.Price);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name.Length, Price);
+        }
+    }
+}
