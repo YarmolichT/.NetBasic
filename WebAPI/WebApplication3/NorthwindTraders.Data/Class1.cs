6fe369531b9e3beec13ee4fd9533dc3c92fcd854
Tatsiana Yarmolich <tatsiana_yarmolich@epam.com>
2023-08-17 18:50:06 +0300

    add WebAPI

---
+++ WebAPI/WebApplication3/NorthwindTraders.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NorthwindTraders.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebAPI/WebApplication3/NorthwindTraders.Data/EmployeeTerritories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindTraders.Data
+{
+    public partial class EmployeeTerritories
+    {
+        [Key]
+        [Column("EmployeeID")]
+        public int EmployeeId { get; set; }
+        [Key]
+        [Column("TerritoryID")]
+        [StringLength(20)]
+        public string TerritoryId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        [InverseProperty(nameof(Data.Employee.EmployeeTerritories))]
+        public virtual Employee Employee { get; set; }
+        [ForeignKey(nameof(TerritoryId))]
+        [InverseProperty(nameof(Data.Territory.EmployeeTerritories))]
+        public virtual Territory Territory { get; set; }
+    }
+}
